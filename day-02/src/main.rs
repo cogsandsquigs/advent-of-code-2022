f1e62d7c2eb10e532a3a77b7e1528a24b01e9224
Ian Pratt <ianjdpratt@gmail.com>
2022-12-01 17:29:59 +0000

    just setup day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,7 +1,12 @@
 use anyhow::Result;
+use utils::files::read_file_string;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let input = read_file_string("day-02/input.txt")?;
+
+    println!("Puzzle 1 answer: {}", todo!());
+
+    println!("Puzzle 2 answer: {}", todo!());
 
     Ok(())
 }
