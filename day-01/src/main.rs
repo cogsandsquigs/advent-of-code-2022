89a35526dbf9a17cf7e46d1ffd40a228387cd00d
Ian Pratt <ianjdpratt@gmail.com>
2022-12-01 17:24:17 +0000

    optimize day 1 puzzle

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -5,37 +5,27 @@ fn main() -> Result<()> {
     let input = read_file_string("day-01/input.txt")?;
 
     // Get the elves' calorie counts
-    let mut elves: Vec<Vec<usize>> = input
+    let mut elves: Vec<usize> = input
         .split("\n\n")
         .map(|elf| {
             elf.split("\n")
                 .map(|count| count.parse::<usize>().unwrap())
-                .collect()
+                .fold(0, |acc, x| acc + x)
         })
         .collect();
 
     // Sort the elves from highest to lowest
     elves.sort_by(|a, b| {
-        let a = a.into_iter().fold(0, |acc, x| acc + x);
-        let b = b.into_iter().fold(0, |acc, x| acc + x);
-
         // Flip from regular `a.cmp(b)` to sort highest to lowest/descending, meaning
         // we get the elf w/ highest calorie count by indexing at 0.
         b.cmp(&a)
     });
 
-    println!(
-        "Puzzle 1 answer: {}",
-        elves[0].clone().into_iter().fold(0, |acc, x| acc + x)
-    );
+    println!("Puzzle 1 answer: {}", elves[0]);
 
     println!(
         "Puzzle 2 answer: {}",
-        elves
-            .clone()
-            .into_iter()
-            .take(3)
-            .fold(0, |acc, x| acc + x.into_iter().fold(0, |acc, x| acc + x))
+        elves.clone().into_iter().take(3).fold(0, |acc, x| acc + x)
     );
 
     Ok(())
