3d238b67e7fa716ab5271f6612a31904511c80d5
Ian Pratt <ianjdpratt@gmail.com>
2022-12-05 21:42:09 +0000

    finish day 5

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,22 +1,16 @@
-use std::collections::HashMap;
-
 use anyhow::Result;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take},
-    character::{
-        complete::{alpha1, digit1, one_of},
-        streaming::char,
-    },
-    error::{ErrorKind, ParseError},
-    multi::{count, separated_list1},
-    sequence::{delimited, pair, separated_pair},
+    character::{complete::digit1, streaming::char},
+    multi::separated_list1,
+    sequence::{delimited, separated_pair},
     IResult,
 };
 use utils::files::read_file_string;
 
 fn main() -> Result<()> {
-    let input = read_file_string("day-05/input.test.txt")?;
+    let input = read_file_string("day-05/input.txt")?;
 
     println!("Puzzle 1 answer: {}", part_1(&input));
 
@@ -25,17 +19,43 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part_2(input: &str) -> usize {
-    todo!()
+fn part_2(input: &str) -> String {
+    let mut supply = Supply::new(input);
+
+    for (amount, from, to) in supply.moves {
+        let from_len = supply.stacks[from - 1].len();
+        let tail = supply.stacks[from - 1].split_off(from_len - amount);
+        supply.stacks[to - 1].extend(tail.into_iter());
+    }
+
+    String::from_iter(
+        supply
+            .stacks
+            .into_iter()
+            .map(|stack| stack.last().unwrap().clone()),
+    )
 }
 
-fn part_1(input: &str) -> usize {
-    let stacks = Stacks::new(input);
+fn part_1(input: &str) -> String {
+    let mut supply = Supply::new(input);
 
-    todo!()
+    for (amount, from, to) in supply.moves {
+        let from_len = supply.stacks[from - 1].len();
+        let mut tail = supply.stacks[from - 1].split_off(from_len - amount);
+        tail.reverse();
+        supply.stacks[to - 1].extend(tail.into_iter());
+    }
+
+    String::from_iter(
+        supply
+            .stacks
+            .into_iter()
+            .map(|stack| stack.last().unwrap().clone()),
+    )
 }
 
-struct Stacks {
+#[derive(Debug)]
+struct Supply {
     stacks: Vec<Vec<char>>,
     moves: Vec<(
         // Number of crates to move
@@ -47,26 +67,60 @@ struct Stacks {
     )>,
 }
 
-impl Stacks {
-    fn new(input: &str) -> Stacks {
-        let stacks: Stacks = Stacks {
+impl Supply {
+    fn new(input: &str) -> Supply {
+        let mut supply: Supply = Supply {
             stacks: vec![],
             moves: vec![],
         };
 
-        let (_, (mut stack_crates, stack_idxs)) = stack_parser(input).unwrap();
+        let (input, (mut stack_crates, _)) = stack_parser(input).unwrap();
 
         // Reverse them so we can traverse them and push/pop them on as needed
         stack_crates.reverse();
 
         for crates in stack_crates {
             for (idx, cr) in crates.into_iter().enumerate() {
-                stacks
+                if supply.stacks.is_empty() || supply.stacks.len() < idx + 1 {
+                    supply.stacks.push(vec![]);
+                }
+
+                let char = cr.chars().next().unwrap();
+
+                if char != ' ' {
+                    supply.stacks[idx].push(char);
+                }
             }
         }
 
-        stacks
+        supply.moves = moves_parser(input).unwrap().1;
+
+        supply
     }
+}
+
+fn moves_parser(input: &str) -> IResult<&str, Vec<(usize, usize, usize)>> {
+    let (input, _) = tag("\n\n")(input)?;
+    separated_list1(tag("\n"), move_parser)(input)
+}
+
+fn move_parser(input: &str) -> IResult<&str, (usize, usize, usize)> {
+    let (input, _) = tag("move ")(input)?;
+
+    let (input, amount_str) = digit1(input)?;
+    let amount: usize = amount_str.parse().unwrap();
+
+    let (input, _) = tag(" from ")(input)?;
+
+    let (input, from_str) = digit1(input)?;
+    let from: usize = from_str.parse().unwrap();
+
+    let (input, _) = tag(" to ")(input)?;
+
+    let (input, to_str) = digit1(input)?;
+    let to: usize = to_str.parse().unwrap();
+
+    Ok((input, (amount, from, to)))
 }
 
 fn stack_parser(input: &str) -> IResult<&str, (Vec<Vec<&str>>, Vec<&str>)> {
