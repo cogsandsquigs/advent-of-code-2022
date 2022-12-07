2aa66e05ec918002af376fd35429d33a55a12268
Ian Pratt <ianjdpratt@gmail.com>
2022-12-07 07:02:14 +0000

    init day 8

---
+++ day-08/src/main.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use utils::files::read_file_string;
+
+fn main() -> Result<()> {
+    let input = read_file_string("day-07/input.txt")?;
+
+    println!("Puzzle 1 answer: {}", part_1(&input));
+
+    println!("Puzzle 2 answer: {}", part_2(&input));
+
+    Ok(())
+}
+
+fn part_2(input: &str) -> usize {
+    todo!()
+}
+
+fn part_1(input: &str) -> usize {
+    todo!()
+}
