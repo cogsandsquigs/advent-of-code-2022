dc4bf6656cf7754d47955142aa9c2f1a8e9263b0
Ian Pratt <ianjdpratt@gmail.com>
2022-12-14 04:59:30 +0000

    added new day

---
+++ day-15/src/main.rs
@@ -0,0 +1,20 @@
+use advent_utils::files::read;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let input = read("day-14/input.test.txt")?;
+
+    println!("Puzzle 1 answer: {}", part_1(&input));
+
+    println!("Puzzle 2 answer: {}", part_2(&input));
+
+    Ok(())
+}
+
+fn part_2(input: &str) -> usize {
+    todo!()
+}
+
+fn part_1(input: &str) -> usize {
+    todo!()
+}
