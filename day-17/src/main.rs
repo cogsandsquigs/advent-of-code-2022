d5e92bd7c9aaa675a61ec5002b853eb45672503d
Ian Pratt <ianjdpratt@gmail.com>
2022-12-17 19:37:04 -0600

    init stuff ig

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -18,5 +18,36 @@ fn part_2(input: &str) -> i64 {
 
 #[solution(day = "17", part = "1")]
 fn part_1(input: &str) -> i64 {
+    let jetstream = Jetstream::new(input);
+
+    print!("{}", 1 >> 1);
+
     todo!()
+}
+
+struct Jetstream {
+    pattern: Vec<i32>,
+    position: usize,
+}
+
+impl Jetstream {
+    pub fn new(input: &str) -> Self {
+        Self {
+            pattern: input
+                .chars()
+                .map(|c| match c {
+                    '>' => 1,
+                    '<' => -1,
+                    c => unreachable!("Bad input: unexpected character '{c}'"),
+                })
+                .collect(),
+            position: 0,
+        }
+    }
+
+    pub fn next(&mut self) -> i32 {
+        let value = self.pattern[self.position];
+        self.position = (self.position + value as usize) % self.pattern.len();
+        value
+    }
 }
