6b261cd5fac58025eea936367a311867dfc3b4c4
Ian Pratt <ianjdpratt@gmail.com>
2022-12-22 05:09:04 +0000

    add day 23

---
+++ day-23/src/main.rs
@@ -0,0 +1,16 @@
+use advent_utils::macros::solution;
+
+fn main() {
+    part_1();
+    part_2();
+}
+
+#[solution(day = "23", part = "2")]
+fn part_2(input: &str) -> i64 {
+    todo!()
+}
+
+#[solution(day = "23", part = "1")]
+fn part_1(input: &str) -> i64 {
+    todo!()
+}
